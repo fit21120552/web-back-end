4b735ff61f58dcf79067b7767019cd6c2afc14ba
21120612 <nmt6465@gmail.com>
2024-01-06 09:41:02 +0700

    khoi tao project

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = express();
+process.noDeprecation = true;
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.static(`${__dirname}/public`));
+
+module.exports = app;
