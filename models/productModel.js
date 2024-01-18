c484206d1a1b19b8d37a0939f767bb444d498103
21120612 <nmt6465@gmail.com>
2024-01-18 10:14:20 +0700

    add product schema

--- models/productModel.js
+++ models/productModel.js
@@ -15,13 +15,39 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       required: [true, "product must have to price"],
     },
-    discountPercentage:{
-        type:String,
-        required:[true]
-    }
+    discountPercentage: {
+      type: String,
+      required: [true],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, "rating must be above 1"],
+      max: [5, "rating must be below 5"],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    images: [String],
+    thumbnail: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: [true, "product must have category"],
+    },
+    brand: {
+      type: String,
+      required: [true, "product must have brand"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "product must have stock"],
+    },
   },
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
   }
 );
+
+const product = mongoose.model("Product", ProductSchema);
+module.exports = product;
