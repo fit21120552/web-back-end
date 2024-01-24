0e2df82512fc84bab778168f16e5930bb2448a0b
21120612 <nmt6465@gmail.com>
2024-01-25 00:11:49 +0700

    stats product relate

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const products = require("./../models/productModel");
 const factory = require("./../db/HandleFactory");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.getAllProduct = factory.getAll(products, { path: "reviews" });
 
@@ -12,3 +13,27 @@ exports.getProduct = factory.getOne(products, { path: "reviews" });
 exports.updateProduct = factory.updateOne(products);
 
 exports.deleteProduct = factory.deleteOne(products);
+
+exports.getRelatedProducts = catchAsync(async (req, res, next) => {
+  const currentProduct = await products.findById(req.params.productId);
+  if (!currentProduct) {
+    return next(new AppError("no product found", 404));
+  }
+  const relatedProducts = await products
+    .find({
+      $and: [
+        { brand: currentProduct.brand },
+        { category: currentProduct.category },
+        { price: { $gte: currentProduct.price - 500, $lte: currentProduct.price + 500 } },
+        { _id: { $ne: currentProduct._id } },
+      ],
+    })
+    .limit(4);
+  res.status(200).json({
+    status: "success",
+    result: relatedProducts.length,
+    data: {
+      data: relatedProducts,
+    },
+  });
+});
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,6 +5,8 @@ const reviewRouter = require("./reviewRoute");
 productRouter.route("/").get(productController.getAllProduct);
 productRouter.route("/:id").get(productController.getProduct);
 
+// stats
+productRouter.route("/related-products/:productId").get(productController.getRelatedProducts);
 // permission admin
 productRouter.route("/create").post(productController.createProduct);
 productRouter.route("/update/:id").patch(productController.updateProduct);
