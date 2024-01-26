349ead1a9ae0fdadc7b52599930df49882cda505
21120612 <nmt6465@gmail.com>
2024-01-26 18:04:51 +0700

    them tu dong them san pham hien co

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -44,7 +44,7 @@ CategorySchema.pre("save", async function (next) {
     return next();
   }
   const ListProduct = await require("./productModel").find({ category: this.name });
-  console.log(ListProduct);
+  if (this.products.length === 0) this.products = ListProduct.map((product) => product._id);
   const uniqueBrands = [...new Set(ListProduct.map((product) => product.brand))];
   this.subCategory = uniqueBrands.map((brand) => ({ name: brand }));
   next();
